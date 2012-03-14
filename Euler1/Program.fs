bcdc377a216753ecce3ebb4052165b74f3651e31
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-03-14 09:48:16 +0100

    First commit

---
+++ Euler1/Program.fs
@@ -0,0 +1,10 @@
+﻿
+[<EntryPoint>]
+let main args =
+
+    let range = seq { for n in 1..999 do if  n % 3 = 0 || n % 5 = 0 then yield n}
+    System.Console.WriteLine (Seq.sum range)
+
+    let toto = System.Console.ReadLine()
+    System.Console.WriteLine toto
+    0
---
+++ Euler3/Program.fs
@@ -0,0 +1,24 @@
+﻿[<EntryPoint>]
+let main args =
+    let valToCompute = 600851475143L
+    let maxValue = System.Convert.ToInt64 (sqrt 600851475143.)
+
+    let rec divWhileDivisible value x =
+        if(value%x=0L) then 
+            divWhileDivisible (value/x) x 
+        else 
+            value
+
+    let folder (curValue, maxDiv) cur =
+        if(curValue%cur=0L) then 
+            divWhileDivisible curValue cur, cur
+        else 
+            curValue, maxDiv
+
+    let result = Seq.fold folder (valToCompute, 1L) {2L..maxValue}
+
+    System.Console.WriteLine result
+
+    let toto = System.Console.ReadLine()
+    System.Console.WriteLine toto
+    0
