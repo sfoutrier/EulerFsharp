c55db6e8699d7464b18e28e6af4e9ea73f60d1bc
Sebastien FOUTRIER <sebastien.foutrier@gmail.com>
2012-03-16 18:31:32 +0100

    Adding solution for problem 6

---
+++ Euler6/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+[<EntryPoint>]
+let main args =
+
+    let maxValue = 100;
+
+    // the loop algo (no clue about the maths formula)
+    let sum1 = Seq.fold (fun acc cur -> acc + cur*cur) 0 (seq { 1..maxValue })
+    let sum2 = maxValue * (maxValue + 1) / 2
+    let result = sum2 * sum2 - sum1
+
+    printfn "%d" result
+
+    let _ = Console.ReadKey true
+    0
