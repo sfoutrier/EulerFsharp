2779232e3767e8686d1388932199013e3806bf27
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-03-18 22:11:20 +0100

    Fix solution for problem 12

--- Euler12/Program.fs
+++ Euler12/Program.fs
@@ -2,17 +2,13 @@
 [<EntryPoint>]
 let main args =
 
-    (*let triangles = Seq.unfold (fun (i, curTriangle) ->
-                                    let nextTriangle = curTriangle + i
-                                    Some (nextTriangle, (i, nextTriangle)) ) (1, 0)*)
+    let triangles = Seq.initInfinite (fun x -> x * (x + 1) / 2)
 
-    let divisors x = seq { for y in 1..x do if x % y = 0 then yield y }
-    let numbersWithDivisors = Seq.unfold (fun acc -> Some((acc, Seq.length (divisors acc)), acc + 1)) 1
-    let consecutivesNumbers = Seq.windowed 2 numbersWithDivisors
-    let consecutiveWithAtLeastNDivisors howMuch = Seq.filter (fun x -> Array.sumBy (fun y -> snd y) x >= howMuch) consecutivesNumbers
-                                                    |> Seq.map (fun x -> (Array.fold (fun acc y -> acc * fst y) 1 x) / 2 )
+    let countDivisors x =
+        Seq.sumBy (function y when x % y = 0 -> 2 | _ -> 0) {1..(int(sqrt(float x)))}
 
-    Seq.iter (fun x -> printfn "%d : %d" x (Seq.length (divisors x))) (consecutiveWithAtLeastNDivisors 40)
+    let result = Seq.find (fun x -> countDivisors x > 500) triangles
+    printfn "%d" result
 
     let _ = System.Console.ReadKey true
     0
