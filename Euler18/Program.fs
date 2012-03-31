3e28b6668d7edb87fa9d12732b1f504716952ec0
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-03-31 15:27:04 +0200

    Adding solution for problem 18

---
+++ Euler18/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO;
+
+[<EntryPoint>]
+let main args =
+    use triangleFile = File.OpenText "../../triangle"
+    let triangle = Seq.unfold (fun (file:StreamReader) -> 
+                                match file.EndOfStream with
+                                | false -> Some(file.ReadLine().Split ' ' |> Array.map System.Int32.Parse, file)
+                                | true -> None) triangleFile |> Seq.toList
+    
+    let rec paths position lines currentPath = 
+        seq {
+            match lines with
+            | [] -> 
+                yield currentPath
+            | x::tail ->
+                yield! paths position tail (Array.get x position :: currentPath)
+                yield! paths (position + 1) tail (Array.get x (position + 1) :: currentPath)
+        }
+
+    let allPaths = paths 0 (List.tail triangle) [(Seq.head (List.head triangle))]
+    let maxPath = Seq.maxBy List.sum allPaths
+    printfn "%d : %A" (List.sum maxPath) maxPath
+
+    let _ = System.Console.ReadKey true
+    0
