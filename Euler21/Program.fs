5def1f03156e47d074f5280819a806c3473015cb
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-04-01 19:16:07 +0200

    Solution to problem 21

---
+++ Euler21/Program.fs
@@ -0,0 +1,26 @@
+﻿// http://projecteuler.net/problem=21
+
+[<EntryPoint>]
+let main args =
+
+    let d n = Seq.sum (seq { for i in 1..n/2 do if (n % i = 0) then yield i })
+    
+    let allAmicableToN n = Seq.fold (
+                                fun curAmicable i ->
+                                    match Set.contains i curAmicable with
+                                    | true -> curAmicable
+                                    | false -> 
+                                        let res1 = d i
+                                        match d res1 with
+                                        | res2 when res2 = i && res1 <> i -> 
+                                            Set.add i (Set.add res1 curAmicable)
+                                        | _ -> curAmicable
+                            ) Set.empty {1..n}
+
+
+    let allAmicableTo10000 = allAmicableToN 10000
+    let result = Seq.sum allAmicableTo10000
+    printfn "%d" result
+
+    let _ = System.Console.ReadKey true
+    0
