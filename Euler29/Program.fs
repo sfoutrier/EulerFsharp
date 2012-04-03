0ccc28c7728132fdd6f2355043137efaba91074b
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-04-04 00:11:00 +0200

    Adding solution for problem 29

---
+++ Euler29/Program.fs
@@ -0,0 +1,18 @@
+﻿// http://projecteuler.net/problem=29
+
+[<EntryPoint>]
+let main args =
+    
+    let res = 
+        Seq.fold
+            ( fun powSet (i, j) ->
+                Set.add (bigint.Pow (bigint(i:int), j)) powSet )
+            Set.empty
+            ( seq { for i in 2..100 do
+                        for j in 2..100 -> (i, j) } )
+        |> Set.count
+
+    printfn "%d" res
+
+    let _ = System.Console.ReadKey true
+    0
