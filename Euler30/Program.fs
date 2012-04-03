4847155cfa020ec72c975e4c564d77e6dbffd420
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-04-04 00:33:23 +0200

    Adding pseudo solution for problem 30

---
+++ Euler30/Program.fs
@@ -0,0 +1,22 @@
+﻿// http://projecteuler.net/problem=30
+
+[<EntryPoint>]
+let main args =
+    
+    let sumFifthPowOfDigits n =
+        Seq.sumBy (fun x -> x * x * x * x * x)
+            ( Seq.unfold (function acc when acc = 0 -> None | acc -> Some(acc % 10, acc / 10) ) n )
+
+    // let run then prey for there is no more match after few seconds ...
+    let _ = 
+        Seq.fold ( fun acc ->
+                    function 
+                    |(Some x) -> printfn "%d" (acc + x); acc + x
+                    | _ -> acc
+                ) 0
+            (Seq.filter (function None -> false | _ -> true)
+                ( Seq.initInfinite ( function 1 -> None | x when x = sumFifthPowOfDigits x -> Some x | _ -> None ) ) 
+            )
+
+    let _ = System.Console.ReadKey true
+    0
