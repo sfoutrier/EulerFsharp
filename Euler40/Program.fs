e9a81b85df065f3f9b34504f328197aaa31345f6
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-04-07 23:36:06 +0200

    Adding solution for problem 40

---
+++ Euler40/Program.fs
@@ -0,0 +1,23 @@
+﻿// http://projecteuler.net/problem=40
+
+[<EntryPoint>]
+let main args =
+
+    let digitsOf =
+        Seq.unfold (function 0 -> None | x -> Some (x % 10, x / 10))
+        >> Seq.toList >> List.rev
+            
+    let setOfPosToKeep =
+        Set.ofList [1;10;100;1000;10000;100000;1000000]
+
+    let seqOfDigits =
+        Seq.initInfinite digitsOf
+        |> Seq.concat
+        |> Seq.mapi (fun i x -> match setOfPosToKeep.Contains (i+1) with false -> None | true -> Some(x))
+        |> Seq.choose id
+        |> Seq.take setOfPosToKeep.Count
+
+    printfn "%d" (Seq.reduce (*) seqOfDigits)
+        
+    let _ = System.Console.ReadKey true
+    0
