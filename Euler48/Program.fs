00571b45ca31feed10c9e17d4f0d9505c4ef157b
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2013-12-23 14:32:31 +0100

    Add solution for pbm 48

---
+++ Euler48/Program.fs
@@ -0,0 +1,21 @@
+﻿[<EntryPoint>]
+let main args =
+    let MAX = 1000L
+    let modulo = 10000000000L
+
+    let lastDigitPow x =
+        Seq.fold
+            (fun acc _ -> (x * acc) % modulo)
+            1L
+            {1L..x}
+
+    let result =
+        Seq.fold
+            (fun acc i -> (acc + (lastDigitPow i)) % modulo)
+            0L
+            {1L..MAX}
+
+    printfn "%d" result
+
+    let _ = System.Console.ReadKey true
+    0
