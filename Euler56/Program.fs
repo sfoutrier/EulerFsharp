b78d18c91422865b686ab34abac8fbfc8f859f6a
Sébastien Foutrier <sebastien.foutrier@gmail.com>
2013-12-27 21:57:45 +0100

    Add solution 56

---
+++ Euler56/Program.fs
@@ -0,0 +1,12 @@
+﻿[<EntryPoint>]
+let main args =
+    let MAX = 100
+
+    seq {for i in 1..MAX do
+            for j in 1..MAX -> bigint.Pow(new bigint(i), j)}
+    |> Seq.map (fun v -> v.ToString() |> Seq.fold (fun s c -> s + int(c) - int('0')) 0)
+    |> Seq.max
+    |> printfn "%A"
+
+    let _ = System.Console.ReadKey true
+    0
